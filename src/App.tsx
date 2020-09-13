40cc75ace5be62728ed4cc20864cfcbb874dd323
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-09-13 10:37:36 +0100

    wrap app in suspense boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React, { lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 
+import SuspenseBoundary from "./commons/boundary/suspense";
+
 const SignUp = lazy(() => import("./pages/signup"));
 const Login = lazy(() => import("./pages/login"));
 
 function App() {
   return (
     <Switch>
-      <Route path="/login" exact>
-        <Login />
-      </Route>
-      <Route path="/signup" exact>
-        <SignUp />
-      </Route>
+      <SuspenseBoundary>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="/signup" exact>
+          <SignUp />
+        </Route>
+      </SuspenseBoundary>
     </Switch>
   );
 }
